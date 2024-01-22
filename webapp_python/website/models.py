c19f2e6767be3cc06d3b72a0a665b249b218271f
Nozatron986 <145579780+Nozatron986@users.noreply.github.com>
2024-01-22 11:18:05 +0000

    Update models.py

--- webapp_python/website/models.py
+++ webapp_python/website/models.py
@@ -13,4 +13,5 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    is_customer = db.Column(db.Boolean, default=True)
     notes = db.relationship('Note')
